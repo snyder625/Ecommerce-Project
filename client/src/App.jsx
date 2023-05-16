3b2aa66457711116b5cc8db8af14dfc888adb9d6
Abdullah Aziz <abdullahazees4@gmail.com>
2023-05-16 20:01:24 +0500

    blog page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,25 +4,36 @@ import { Cart, Dashboard, Home, Login, Menu, Product, Register } from "./pages";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Contact from "./components/contactUs/Contact";
+import BlogMain from "./components/Blog/BlogMain";
+import BlogViewDetails from "./components/Blog/BlogViewDetails";
+import ScrollTop from "./scroll/ScrollTop";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="menu" element={<Menu />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="admin" element={<Dashboard />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="product">
-            <Route path=":id" element={<Product />} />
-          </Route>
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="/blog" />
-        </Routes>
+        <ScrollTop>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="menu" element={<Menu />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="admin" element={<Dashboard />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="product">
+              <Route path=":id" element={<Product />} />
+            </Route>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+
+            <Route exact path="/blog" element={<BlogMain />} />
+            <Route
+              exact
+              path="/blog/blogdata/:id"
+              element={<BlogViewDetails />}
+            />
+          </Routes>
+        </ScrollTop>
         <Footer />
       </BrowserRouter>
     </>
