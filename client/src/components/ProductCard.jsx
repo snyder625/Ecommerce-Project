5d76010429241b7ec28bf3899c661ba491c9e3dc
Abdullah Aziz <abdullahazees4@gmail.com>
2023-05-29 23:43:15 +0500

    menu ui updated

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -3,35 +3,39 @@ import styles from "../styles/ProductCard.module.css"; // Import the CSS file fo
 const ProductCard = ({ product }) => {
   return (
     <div className={styles.productCard}>
-      <div className={styles.productImage}>
-        <img src={product.image} alt="Product" className={styles.Image} />
-      </div>
-      <div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            margin: "10px 0",
-            alignItems: "center",
-          }}
-        >
-          <h3 className={styles.productName}>{product.name}</h3>
-          <h3 className={styles.productPrice}>
-            <sub>Rs. 4900</sub>
-          </h3>
-        </div>
-        <p className={styles.productDescription}>{product.description}</p>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            marginBottom: 10,
-          }}
-        >
-          <button className={styles.cartButton}>Add to Cart</button>
-        </div>
-      </div>
+      {product && (
+        <>
+          <div className={styles.productImage}>
+            <img src={product.image} alt="Product" className={styles.Image} />
+          </div>
+          <div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                margin: "10px 0",
+                alignItems: "center",
+              }}
+            >
+              <h3 className={styles.productName}>{product.name}</h3>
+              <h3 className={styles.productPrice}>
+                <sub>Rs. 4900</sub>
+              </h3>
+            </div>
+            <p className={styles.productDescription}>{product.description}</p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                marginBottom: 10,
+              }}
+            >
+              <button className={styles.cartButton}>Add to Cart</button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
