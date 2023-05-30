5831cb962aeb7bd7035d6785ce1c1c38f95db7af
Rizwan Ahmed <75876203+snyder625@users.noreply.github.com>
2023-05-30 06:14:19 +0500

    Update ProductCard.jsx

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -3,41 +3,10 @@ import styles from "../styles/ProductCard.module.css"; // Import the CSS file fo
 const ProductCard = ({ product }) => {
   return (
     <div className={styles.productCard}>
-<<<<<<< HEAD
-      <div className={styles.productImage}>
-        <img src={product.img} alt="Product" className={styles.Image} />
-      </div>
-      <div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            margin: "10px 0",
-            alignItems: "center",
-          }}
-        >
-          <h3 className={styles.productName}>{product.title}</h3>
-          <h3 className={styles.productPrice}>
-            <sub>Rs. 4900</sub>
-          </h3>
-        </div>
-        <p className={styles.productDescription} aria-rowspan={4}>{product.desc}</p>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            marginBottom: 10,
-          }}
-        >
-          <button className={styles.cartButton}>{product.category === "pizza" ? "Customize" : "Add to Cart"}</button>
-        </div>
-      </div>
-=======
       {product && (
         <>
           <div className={styles.productImage}>
-            <img src={product.image} alt="Product" className={styles.Image} />
+            <img src={product.img} alt="Product" className={styles.Image} />
           </div>
           <div>
             <div
@@ -49,12 +18,12 @@ const ProductCard = ({ product }) => {
                 alignItems: "center",
               }}
             >
-              <h3 className={styles.productName}>{product.name}</h3>
+              <h3 className={styles.productName}>{product.title}</h3>
               <h3 className={styles.productPrice}>
-                <sub>Rs. 4900</sub>
+                <sub>Rs. {product.prices[0]}</sub>
               </h3>
             </div>
-            <p className={styles.productDescription}>{product.description}</p>
+            <p className={styles.productDescription}>{product.desc}</p>
             <div
               style={{
                 display: "flex",
@@ -67,7 +36,6 @@ const ProductCard = ({ product }) => {
           </div>
         </>
       )}
->>>>>>> e9851a7e292a339576852c5d4dfa60a1206cbcae
     </div>
   );
 };
