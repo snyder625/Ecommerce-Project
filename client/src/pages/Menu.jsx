e0ebe4ada06016c497483a34b633295bc4c2246f
Rizwan Ahmed <rizwanahmed060602@gmail.com>
2023-06-14 21:06:32 +0500

    Menu Page Updated

--- client/src/pages/Menu.jsx
+++ client/src/pages/Menu.jsx
@@ -1,23 +1,32 @@
 import { useEffect, useState } from "react";
 import styles from "../styles/Menu.module.css";
+// import { useSelector, useDispatch } from "react-redux";
+// import Loading from "../layout/loader/Loading";
+import Pagination from "react-js-pagination";
 import Slider from "@mui/material/Slider";
 import axios from "axios";
 import { BASE_URL } from "../urlHandler";
 
 import Typography from "@mui/material/Typography";
+import { useParams } from "react-router-dom";
 import ProductCard from "../components/ProductCard";
+import { useSelector } from "react-redux";
 
 const categories = ["All", "Pizza", "Burger", "Side", "Dessert"];
 
 const Menu = () => {
-
+  // const dispatch = useDispatch();
+  // const alert = useAlert();
   const [currentPage, setCurrentPage] = useState(1);
   const [price, setprice] = useState([0, 5000]);
+  const [ratings, setRatings] = useState(0);
   const [category, setCategory] = useState("");
   const [productsData, setProducts] = useState([]);
 
   const [keyword, setKeyword] = useState("");
 
+  // const { products, loading, error, productsCount, resultPerPage } =
+  // useSelector((state) => state.products);
   const setCurrentPageNum = (e) => {
     setCurrentPage(e);
   };
@@ -34,9 +43,6 @@ const Menu = () => {
     console.log("Selected category's type is", typeof checkboxValue);
   };
 
-  const resultPerPage = 10;
-  const productsCount = 10;
-
   const getAllProducts = async () => {
     let result = await axios.get(
       `${BASE_URL}/products?minPrice=${price[0]}&maxPrice=${price[1]}`
@@ -46,10 +52,15 @@ const Menu = () => {
         `${BASE_URL}/products?minPrice=${price[0]}&maxPrice=${price[1]}`
       );
     }
-    if (category !== "" && category != "all") {
+    if (category !== "" && category !== "all") {
       let categoryLowercase = category.toLowerCase();
       result = await axios.get(
-        `${BASE_URL}/products?category=${categoryLowercase}`
+        `${BASE_URL}/products?category=${categoryLowercase}&minPrice=${price[0]}&maxPrice=${price[1]}`
+      );
+    }
+    if (keyword !== "") {
+      result = await axios.get(
+        `${BASE_URL}/products?keyword=${keyword}&minPrice=${price[0]}&maxPrice=${price[1]}`
       );
     }
     console.log("result" + result);
@@ -63,6 +74,14 @@ const Menu = () => {
   const loading = false;
   console.log(keyword);
 
+  // useEffect(() => {
+  //   // if (error) {
+  //   //   alert.error(error);
+  //   //   dispatch(clearErrors());
+  //   // }
+  //   // dispatch(getProduct(keyword, currentPage, price, category, ratings));
+  // }, []);
+  // let count=filteredproductscount;
   return (
     <>
       {loading ? (
@@ -132,6 +151,25 @@ const Menu = () => {
               ))}
             </div>
           </div>
+
+          {/*resultPerPage < productsCount && (
+            <div className={styles.paginationBox}>
+              <Pagination
+                activePage={currentPage}
+                itemsCountPerPage={resultPerPage}
+                totalItemsCount={productsCount}
+                onChange={setCurrentPageNum}
+                nextPageText="Next"
+                prevPageText="Prev"
+                firstPageText="1st"
+                lastPageText="Last"
+                itemClass="page-item"
+                linkClass="page-link"
+                activeClass="pageItemActive"
+                activeLinkClass="pageLinkActive"
+              />
+            </div>
+          )*/}
         </>
       )}
     </>
